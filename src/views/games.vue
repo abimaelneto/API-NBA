0fbbb209d4bf9fb723996fe71e99eae678f23c6f
Douglas Amaral <douglaxz@hotmail.com>
2023-06-13 09:10:57 -0300

    comentando funções

--- src/views/games.vue
+++ src/views/games.vue
@@ -24,27 +24,42 @@ export default {
           this.loading = false;
         });
     },
-    /*função para o botão retornar página*/
+    /*formatar data recebida pela api*/
+    formatarData(date) {
+      const dataObjeto = new Date();
+
+      const dia = dataObjeto.getDate().toString().padStart(2, "0");
+      const mes = (dataObjeto.getMonth() + 1).toString().padStart(2, "0");
+      const ano = dataObjeto.getFullYear();
+      const hora = dataObjeto.getHours().toString().padStart(2, "0");
+      const minuto = dataObjeto.getMinutes().toString().padStart(2, "0");
+
+      const dataFormatada = `${dia}/${mes}/${ano} - ${hora}:${minuto}`;
+
+      return dataFormatada;
+    },
+
+    /*função para o botão voltar*/
     handlePrevious() {
       this.getGames(
         `https://www.balldontlie.io/api/v1/games?page=${this.previous}`
       );
     },
-    /*função para o botão avançar página*/
+    /*função para o botão proximo*/
     handleNext() {
       this.getGames(
         `https://www.balldontlie.io/api/v1/games?page=${this.next}`
       );
     },
-    /*função de retorno caso não ache foto no diretório com id*/
+    /*função para caso não tenha foto trocar a imagem por nophoto*/
     replaceByDefault(e) {
       e.target.src = nophoto;
     },
-    /*função procurar imagem com o mesmo nome do id*/
+    /*função para pegar imagem no diretório*/
     getImagePath(id) {
       return `/teams/${id}.png`;
     },
-    /*função para procurar imagem no diretório*/
+    /*função para verificar se a imagem existe*/
     fileExists(filename) {
       var http = new XMLHttpRequest();
 
@@ -64,6 +79,7 @@ export default {
     },
   },
   computed: {
+    /*função para chamar a função de data */
     parsedGames() {
       return this.games.map((el) => ({
         ...el,
